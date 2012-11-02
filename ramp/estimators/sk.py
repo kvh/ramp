975f0bb01251df93eccb46d62a44d9dd4f367552
Ken Van Haren <kenvanharen@gmail.com>
2012-11-02 02:00:26 -0700

    Generic estimator probabilities wrapper

--- ramp/estimators/sk.py
+++ ramp/estimators/sk.py
@@ -1,1 +1,17 @@
 from sklearn import hmm
+
+
+class Probabilities(object):
+    """ wraps a scikit learn estimator to return probabilities (if
+    it supports it)
+    """
+    def __init__(self, estimator):
+        self.estimator = estimator
+
+    def fit(self, x, y):
+        self.estimator.fit(x,y)
+
+    def predict(self, x):
+        probs = self.estimator.predict_proba(x)
+        probs = [p[1] for p in probs]
+        return probs
