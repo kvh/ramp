77ac4ee4eebb0c0ba093326e03c1c5addf7d0e2d
Ken Van Haren <kenvanharen@gmail.com>
2012-11-18 13:42:59 -0800

    Add manifest, authors. Version bump.

--- setup.py
+++ setup.py
@@ -5,26 +5,22 @@ f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
 long_description = f.read()
 f.close()
 
-version = '0.1'
+version = '0.1.3'
 
-setup(name='ramp',
-      version=version,
-      description="Rapid machine learning prototyping",
-      long_description=long_description,
-      classifiers=[
-          'License :: OSI Approved :: BSD License'
-      ],
-      keywords='machine learning data analysis statistics mining',
-      author='Ken Van Haren',
-      author_email='kvh@science.io',
-      url='http://github.com/kvh/ramp',
-      license='BSD',
-      packages=find_packages(exclude=["*.tests"]),
-      zip_safe=False,
-      install_requires=[
-          'numpy',
-          'pandas',
-          'scikit-learn',
-      ]
-      )
+setup(
+    name='ramp',
+    version=version,
+    description="Rapid machine learning prototyping",
+    long_description=long_description,
+    classifiers=[
+        'License :: OSI Approved :: BSD License'
+    ],
+    keywords='machine learning data analysis statistics mining',
+    author='Ken Van Haren',
+    author_email='kvh@science.io',
+    url='http://github.com/kvh/ramp',
+    license='BSD',
+    packages=find_packages(exclude=["*.tests"]),
+    zip_safe=False
+)
 
