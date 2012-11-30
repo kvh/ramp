1c90d70559127a7272ee49592a7a611691e5dccc
Ken Van Haren <kenvanharen@gmail.com>
2012-11-29 18:27:13 -0800

    Adding estimator tests

---
+++ ramp/tests/test_estimators.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append('../..')
+from ramp.estimators import sk
+from ramp import metrics
+from ramp import *
+import unittest
+from pandas import *
+import tempfile
+from ramp import shortcuts
+from ramp.tests.test_features import make_data
+
+
+class DummyProbEstimator(object):
+    def __init__(self, n_clses):
+        self.n_clses = n_clses
+
+    def fit(self, x, y):
+        pass
+
+    def predict_proba(self, x):
+        return np.zeros((len(x), self.n_clses))
+
+
+class TestSKEstimators(unittest.TestCase):
+    def setUp(self):
+        self.data = make_data(10)
+
+    def test_probabilities(self):
+        est = sk.Probabilities(DummyProbEstimator(2))
+        preds, x, y = shortcuts.predict(
+                store=store.MemoryStore(),
+                data=self.data, model=est,
+                predict_index=self.data.index,
+                target='y', metrics=[metrics.AUC()], features=['a'])
+        self.assertEqual(preds.shape, (10, 2))
+
+    def test_binary_probabilities(self):
+        est = sk.BinaryProbabilities(DummyProbEstimator(2))
+        preds, x, y = shortcuts.predict(
+                store=store.MemoryStore(),
+                data=self.data, model=est,
+                predict_index=self.data.index,
+                target='y', metrics=[metrics.AUC()], features=['a'])
+        self.assertEqual(len(preds), 10)
+        t = np.zeros(10)
+        t[0] = 1
+        metrics.AUC().score(t, preds)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
--- ramp/tests/test_store.py
+++ ramp/tests/test_store.py
@@ -4,6 +4,7 @@ from ramp.store import *
 import unittest
 from pandas import *
 import tempfile
+from pandas.util.testing import assert_almost_equal
 
 
 class TestStore(unittest.TestCase):
@@ -23,7 +24,7 @@ class TestStore(unittest.TestCase):
             print "Testing store:", store.__class__.__name__
 
             # keys can be arbitrary strings
-            key = 'hi there-342 {{ ok }}'
+            key = 'hi there-342 {{ ok }} /<>/'
             store.save(key, 1)
             self.assertEqual(store.load(key), 1)
 
@@ -36,7 +37,7 @@ class TestStore(unittest.TestCase):
 
             # test pandas object
             store.save(key, DataFrame([range(10), range(10)]))
-            self.assertEqual(store.load(key), DataFrame([range(10), range(10)]))
+            assert_almost_equal(store.load(key), DataFrame([range(10), range(10)]))
 
             # test miss
             self.assertRaises(KeyError, store.load, ('not'))
