8dddb0ec3fb736c28917a5f08f614fddd074f076
Ken Van Haren <kenvanharen@gmail.com>
2012-11-29 18:23:27 -0800

    Explicit binary probability estimator.

--- ramp/estimators/sk.py
+++ ramp/estimators/sk.py
@@ -2,11 +2,18 @@ from sklearn import hmm
 
 
 class Probabilities(object):
-    """ wraps a scikit learn estimator to return probabilities (if
+    """
+    Wraps a scikit learn estimator to return probabilities (if
     it supports it)
     """
-    def __init__(self, estimator):
+    def __init__(self, estimator, binary=False):
+        """
+        binary: If True, predict returns only the probability
+            for the positive class. If False, returns probabilities for
+            all classes.
+        """
         self.estimator = estimator
+        self.binary = binary
 
     def __str__(self):
         return u"Probabilites for %s" % self.estimator
@@ -16,4 +23,11 @@ class Probabilities(object):
 
     def predict(self, x):
         probs = self.estimator.predict_proba(x)
+        if self.binary:
+            return [p[1] for p in probs]
         return probs
+
+
+class BinaryProbabilities(Probabilities):
+    def __init__(self, estimator):
+        super(BinaryProbabilities, self).__init__(estimator, binary=True)
