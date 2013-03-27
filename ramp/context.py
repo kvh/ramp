f932ace6b40ca8b81a768d9fefe2de864352cc6b
Rob Story <wrobstory@gmail.com>
2013-03-27 06:10:54 -0700

    Started cleaning up docstring, making things a little more Pythonic

--- ramp/context.py
+++ ramp/context.py
@@ -1,3 +1,12 @@
+  # -*- coding: utf-8 -*-
+'''
+DataContext
+-------
+
+The environment of the analysis. 
+
+'''
+
 from hashlib import md5
 import copy
 from utils import get_np_hashable
@@ -14,30 +23,52 @@ class DataContext(object):
     and pandas index value, Ramp will consider the data immutable --
     it will not check the data again to see if it has changed.
     """
-    def __init__(self, store=None, data=None, train_index=None, prep_index=None):
+    def __init__(self, store=None, data=None, train_index=None, 
+                 prep_index=None):
         """
-        **Args**
+        The DataContext is the data storage environment for the Ramp analysis.
+        For a given store and pandas index value, Ramp will consider the data 
+        immutable, and will not check for changes in the data. 
+        
+        Parameters: 
+        -----------
 
-        store: An instance of `store.Store` or a path. If a path
-                Ramp will default to an `HDFPickleStore` at that path
-                if PyTables is installed, a `PickleStore` otherwise.
-                Defaults to MemoryStore.
-        data: a pandas DataFrame. If all data has been precomputed this
-                may not be required.
-        train_index: a pandas Index specifying the data instances to be
-                used in training. Stored results will be cached against this.
-                If not provided, the entire index of `data` will be used.
-        prep_index: a pandas Index specifying the data instances to be
-                used in prepping ("x" values). Stored results will be cached against this.
-                If not provided, the entire index of `data` will be used.
+        store: string or store.Store, default None
+            An instance of `store.Store` or a path. If a path, Ramp will 
+            default to an `HDFPickleStore` at that path if PyTables is 
+            installed, a `PickleStore` otherwise. Defaults to MemoryStore.
+        data: Pandas DataFrame, default None
+            Dataframe of data. If all data has been precomputed this
+            may not be required.
+        train_index: Pandas DataFrame Index, default None
+            Pandas Index specifying the data instances to be used in training. 
+            Stored results will be cached against this.If not provided, the 
+            entire index of the 'data' parameter will be used.
+        prep_index: Pandas DataFrame Index, default None
+            Pandas Index specifying the data instances to be
+            used in prepping ("x" values). Stored results will be cached 
+            against this. If not provided, the entire index of `data` 
+            keyword arg will be used.
+            
         """
         if store is None:
             self.store = MemoryStore()
         else:
             self.store = store if isinstance(store, Store) else default_store(store)
         self.data = data
-        self.train_index = train_index if train_index is not None else self.data.index if self.data is not None else None
-        self.prep_index = prep_index if prep_index is not None else self.data.index if self.data is not None else None
+        if train_index:
+            self.train_index = train_index
+        elif self.data: 
+            self.train_index = self.data.index
+        else: 
+            self.train_index = None
+        
+        if prep_index: 
+            self.prep_index = prep_index
+        elif self.data: 
+            self.prep_index = prep_index
+        else: 
+            self.prep_index = None
 
     def copy(self):
         return copy.copy(self)
